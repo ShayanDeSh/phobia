226fbca66b049bcd01001a3dc7b82a52f82ad1fe
shayandesh <shafaghi1378@gmail.com>
2022-07-14 21:55:27 +0430

    Basic Initializations

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::Deserialize;
+
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[derive(Deserialize, Debug)]
+pub struct Record {
+    method: String,
+    host: String,
+    start: u32,
+    end: u32,
+    #[serde(flatten)]
+    body: Body
+}
+
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "content-type", content = "body")]
+pub enum Body {
+    #[cfg(__YES__)]
+    #[serde(rename(deserialize = "json"))]
+    JSON,
+    #[cfg(__YES__)]
+    #[serde(rename(deserialize = "form"))]
+    FORM,
+    #[serde(rename(deserialize = "multipart"))]
+    MULTIPART{path: String}
 }
