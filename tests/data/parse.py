57b418b5b6bb8ea0b8d06b386dbe1b64e352c51d
shayandesh <shafaghi1378@gmail.com>
2022-07-14 14:57:40 +0430

    Diferreintial time

--- tests/data/parse.py
+++ tests/data/parse.py
@@ -7,10 +7,14 @@ apis = ["/sphinx/v1/predict", "/sphinx/v2/predict", "/sphinx/v3/predict",
 
 def main():
     df = pd.read_excel("./data_6.1~6.30_.xlsx")
-    df.loc[df["date"] == 1].sort_values(by=["user id", "start time"])\
+    df.loc[df["date"] == 1].sort_values(by=["start time", "end time"])\
       .drop(["date", "month", "location(latitude/lontitude)", "user id"],
             axis=1)\
+      .sub(df.min()['start time'])\
       .assign(path=lambda x: apis[random.randint(0, len(apis) - 1)])\
+      .assign(step=1000)\
+      .assign(scale=20)\
+      .assign(method="POST")\
       .to_json("./data.json", "records", indent=4)
 
 
